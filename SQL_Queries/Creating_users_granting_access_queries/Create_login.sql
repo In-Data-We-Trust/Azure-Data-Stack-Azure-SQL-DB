a0548c7daa263a6ee0f53df01632701af068f338
Pratik Patel <pratik@InDataWeTrust.co.uk>
2022-10-08 12:31:41 +0100

    updated workflow to use publish & added table sql

--- SQL_Queries/Creating_users_granting_access_queries/Create_login.sql
+++
@@ -1,7 +0,0 @@
--- =============================================================================================================================
--- Create SQL Login template for Azure SQL Database, Azure Synapse Analytics Database, and Azure Synapse SQL Analytics on-demand
--- =============================================================================================================================
-
-CREATE LOGIN [Pratik]
-	WITH PASSWORD = 'insert_password_here' 
-GO
---
+++ database_ddls/test_first_table_1.sql
@@ -0,0 +1,4 @@
+Create TABLE [dbo].[test_first_table]
+    (
+    column_3 varchar(10)
+    )
